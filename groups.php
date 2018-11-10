f8d5d19668d472b981f7e9d4e85bf18a1ae264c0
egamipeaks <egamipeaks@gmail.com>
2018-11-10 11:51:23 -0600

    Update groups.php

--- groups.php
+++ groups.php
@@ -1,31 +1,37 @@
 <?php
 
-class Groups_User {
-	public $user;
-	public function __contruct( $user_id ) {
-		$this->user = get_user_by( "id", $user_id );
+if ( ! class_exists( 'Groups_User' ) ) {
+	class Groups_User {
+		public $user;
+		public function __contruct( $user_id ) {
+			$this->user = get_user_by( "id", $user_id );
+		}
 	}
 }
 
-class Groups_Group {
-	public static function read( $group_id ) {
-		global $wpdb;
-		$group_table = "{$wpdb->prefix}groups_group";
-		return $wpdb->get_row( $wpdb->prepare(
-			"SELECT * FROM {$group_table} WHERE group_id = %d",
-			$group_id
-		));
+if ( ! class_exists( 'Groups_Group' ) ) {
+	class Groups_Group {
+		public static function read( $group_id ) {
+			global $wpdb;
+			$group_table = "{$wpdb->prefix}groups_group";
+			return $wpdb->get_row( $wpdb->prepare(
+				"SELECT * FROM {$group_table} WHERE group_id = %d",
+				$group_id
+			));
+		}
 	}
 }
 
-class Groups_User_Group {
-	public static function read( $user_id, $group_id ) {
-		global $wpdb;
-		$user_group_table = "{$wpdb->prefix}groups_user_group";
-		return $wpdb->get_row( $wpdb->prepare(
-			"SELECT * FROM {$user_group_table} WHERE user_id = %d AND group_id = %d",
-			$user_id,
-			$group_id
-		));
+if ( ! class_exists( 'Groups_User_Group' ) ) {
+	class Groups_User_Group {
+		public static function read( $user_id, $group_id ) {
+			global $wpdb;
+			$user_group_table = "{$wpdb->prefix}groups_user_group";
+			return $wpdb->get_row( $wpdb->prepare(
+				"SELECT * FROM {$user_group_table} WHERE user_id = %d AND group_id = %d",
+				$user_id,
+				$group_id
+			));
+		}
 	}
 }
